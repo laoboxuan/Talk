c57f3e395a758ae55cb0d0b6e9d28cb8907c8209
Eric <ericguo@faevatech.com>
2016-07-18 15:18:29 +0800

    update eric

---
+++ app.js
@@ -0,0 +1,35 @@
+﻿var express = require('express'),
+    favicon = require('serve-favicon'),
+    html = require('html'),
+    app = express();
+
+var socket = require('./lib/js/socket');
+var handleTalk = require('./lib/js/handleTalk');
+
+app.use(favicon(__dirname + '/image/favicon.ico'));
+app.use('/', express.static(__dirname + '/lib/www'));
+
+
+app.get('/image/:fileName',function(req,res){
+
+    var fileName = req.params.fileName;
+    var filePath = __dirname + '/image/' + fileName;
+    res.sendFile(filePath);
+});
+
+app.get('/css/:fileName',function(req,res){
+
+    var fileName = req.params.fileName;
+    var filePath = __dirname + '/css/' + fileName;
+    res.sendFile(filePath);
+});
+
+app.get('/js/:fileName',function(req,res){
+
+    var fileName = req.params.fileName;
+    var filePath = __dirname + '/lib/js/' + fileName;
+    res.sendFile(filePath);
+});
+
+app.listen(1335);
+console.log("start,1335");
