217c9f02397bdc0a69ee77a5f8a5fa3e95bcf7df
Eric <ericguo@faevatech.com>
2016-07-19 18:41:51 +0800

    eric update

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 ﻿var express = require('express'),
     favicon = require('serve-favicon'),
-    html = require('html'),
+    bodyParser = require('body-parser'),
+    querystring = require('querystring'),
     app = express();
 
+var userJson = require('./lib/db/user.json');
 var socket = require('./lib/js/socket');
 var handleTalk = require('./lib/js/handleTalk');
 
 app.use(favicon(__dirname + '/image/favicon.ico'));
 app.use('/', express.static(__dirname + '/lib/www'));
 
+app.use(bodyParser.text({type: 'text/xml'}));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 
 app.get('/image/:fileName',function(req,res){
 
@@ -29,6 +34,35 @@ app.get('/js/:fileName',function(req,res){
     var fileName = req.params.fileName;
     var filePath = __dirname + '/lib/js/' + fileName;
     res.sendFile(filePath);
+});
+
+
+app.get('/login_success/:userName/:token',function(req,res){
+
+    var userName = req.params.userName;
+    var token = req.params.token;
+    console.log("userName:" + userName);
+    console.log("token:" + token);
+    var filePath = __dirname + '/lib/www/talkPage.html';
+    res.sendFile(filePath);
+});
+
+
+app.post('/login',function(req,res){
+
+    console.log("login");
+    var body = req.body;
+    var nickName = body.nickName;
+    var passWord = body.passWord;
+    if(userJson[nickName] != undefined){
+        if(userJson[nickName] === passWord){
+            res.send({"code" : 200,"msg" : {"nickName" : nickName,"token" : passWord}});
+        }else{
+            res.send({"code" : 6500,"errMsg" : "passWord Error"});
+        }
+    }else{
+        res.send({"code" : 6500,"errMsg" : "nickName Error"});
+    }
 });
 
 app.listen(1335);
